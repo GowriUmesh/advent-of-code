a387a889a326840e66011a4953c9106d5ae55e33
GowriUmesh <mailgowriumesh@gmail.com>
2026-01-12 19:12:38 +0100

    add second method

--- day-01/README.md
+++ day-01/README.md
@@ -9,7 +9,27 @@
 The puzzle simulates a **circular dial numbered 0–99**, starting at position `50`.  
 We are given a series of rotations, each consisting of a direction (`L` for left, `R` for right) and a distance.  
 
-The **twist**: the password is **not the final position**, but the **number of times the dial points at 0** during the sequence.
+The **twist**: the password is **not the final position**, but the **number of times the dial points at 0** during the sequence based of two different methods.
+
+## Examples 
+
+Given these rotations: 
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+
+* Method 0: Count only the final position after each rotation.
+Result: 3 times at 0.
+
+* Method 1 (0x434C49434B): Count every click that passes 0.
+Result: 6 times at 0.
 
 ## 💻 Usage
 
@@ -17,4 +37,8 @@ The **twist**: the password is **not the final position**, but the **number of t
 2. Run the solution script with the input file as an argument:
 
 ```bash
+# Default method (method 0)
 ./secret_entrance.py input.txt
+
+# Method 1 (count every click, 0x434C49434B)
+./secret_entrance.py input.txt --method 1
--- day-01/secret_entrance.py
+++ day-01/secret_entrance.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """
 Advent of Code - Day 1: Secret Entrance
-Count how many times the dial points at 0 after any rotation.
-
+Part 1 : Count how many times the dial points at 0 after any rotation.
+Part 2 : method 0x434C49434B - Count the number of times any click causes 
+the dial to point at 0, regardless of whether it happens during a rotation or
+at the end of a rotation.
 The dial starts at 50 and rotates based on instructions (L/R + distance).
 Dial is circular (0-99, wraps around).
 
 """
 import sys
 from pathlib import Path
+import argparse
 
-def count_dial_zeros(input_file):
+def count_dial_zeros(input_file,method=0):
     # Start position
     position = 50
     count_zero = 0
@@ -25,31 +28,40 @@ def count_dial_zeros(input_file):
         
         direction = line[0]
         distance = int(line[1:])
-        
+        step = -1 if direction == 'L' else 1
 
-        if direction == 'L':
-            position = (position - distance) % 100
-        elif direction == 'R':
-            position = (position + distance) % 100
+        if method == 0:
+            if direction == 'L':
+                position = (position - distance) % 100
+            elif direction == 'R':
+                position = (position + distance) % 100
+            else:
+                raise ValueError(f"Invalid direction: {direction}")
+
+            if position == 0:
+                count_zero += 1
+        elif method == 1:
+            for _ in range(distance):
+                position = (position + step) % 100
+                if position == 0:
+                    count_zero += 1
         else:
-            raise ValueError(f"Invalid direction: {direction}")
-
-        if position == 0:
-            count_zero += 1
-    
+            raise ValueError(f"Invalid method: {method}")
     return count_zero
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 2:
-        print("Usage: python secret_entrance.py <input_file>")
-        sys.exit(1)
-    
-    input_file = sys.argv[1]
-    if not Path(input_file).exists():
-        print(f"Error: File '{input_file}' does not exist.")
-        sys.exit(1)
+    parse = argparse.ArgumentParser(description="Count Zeros.")
+    parse.add_argument("input_file", type=Path,help="Path to the file with input.")
+    parse.add_argument("--method", type=int, choices=[0, 1], default=0,
+                       help="Choose method 0 : to count how many times the dial points at 0 after any rotation." \
+                       "Choose method 1: to count how many times the dial points at 0 during any click (method 0x434C49434B) .")
+    args = parse.parse_args()
 
-    result = count_dial_zeros(input_file)
+    if not args.input_file.exists():
+        print(f"Error: File '{args.input_file}' does not exist.")
+        sys.exit(1)    
+
+    result = count_dial_zeros(args.input_file, args.method)
     print(f"Password (times dial points at 0): {result}")
